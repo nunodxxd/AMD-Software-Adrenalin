65aa3ad36257e7611c325eaf8676b1cbe17d6636
NunoDxxD <41538139+nunodxxd@users.noreply.github.com>
2022-11-20 23:37:30 +0000

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,12 +1,11 @@
-import sys
 import requests
 from bs4 import BeautifulSoup
 
-html = requests.get('https://www.amd.com/en/support')
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.7>
+html = requests.get('https://www.amd.com/en/support',headers=headers).content
 
 soup = BeautifulSoup(html, 'html.parser')
-a = soup.select('#buttons > div > a')
-link= a[href]
+a = soup.select('#buttons > div > a')[0]
+link = a.get('href')
 
-sys.stdout.write(link)
-sys.stdout.flush()
+print(link)
