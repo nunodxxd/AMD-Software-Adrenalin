add05d5b0a8336d57c6adea94d945f73c2ba5bfa
NunoDxxD <41538139+nunodxxd@users.noreply.github.com>
2022-11-20 23:04:07 +0000

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,12 +1,12 @@
 import sys
+import requests
 from bs4 import BeautifulSoup
 
-def clean(txt):
-    soup = BeautifulSoup(txt, 'html.parser')
-    return soup.select('#buttons > div > a')[href]
+html = requests.get('https://www.amd.com/en/support')
 
+soup = BeautifulSoup(html, 'html.parser')
+a = soup.select('#buttons > div > a')
+link= a[href]
 
-if __name__ == "__main__":
-    cleaned = clean(sys.stdin.read())
-    sys.stdout.write(cleaned)
-    sys.stdout.flush()
+sys.stdout.write(cleaned)
+sys.stdout.flush()
