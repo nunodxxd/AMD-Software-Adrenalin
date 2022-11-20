cd87857e189516fa8003a256b7a1d470d4373d9e
NunoDxxD <41538139+nunodxxd@users.noreply.github.com>
2022-11-20 23:39:32 +0000

    Update clean.py

--- clean.py
+++ clean.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.7>
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
 html = requests.get('https://www.amd.com/en/support',headers=headers).content
 
 soup = BeautifulSoup(html, 'html.parser')
