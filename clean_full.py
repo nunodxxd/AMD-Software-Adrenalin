e8f7202db5b984b856dcf6c5f41e7d6a16a2f994
NunoDxxD <41538139+nunodxxd@users.noreply.github.com>
2022-11-22 21:19:34 +0000

    Create clean_full.py

---
+++ clean_full.py
@@ -0,0 +1,12 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
+html = requests.get('https://www.amd.com/en/support/kb/release-notes/rn-rad-win-'+sys.argv[1],headers=headers).content
+
+soup = BeautifulSoup(html, 'html.parser')
+a = soup.select('a[href*='/drivers/whql']')
+link = a.get('href')
+
+print(link)
