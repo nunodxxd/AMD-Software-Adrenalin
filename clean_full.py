5eb15bbc3d79eefd640f7d71da42d4f7993f09a9
NunoDxxD <41538139+nunodxxd@users.noreply.github.com>
2022-11-22 21:51:14 +0000

    Update clean_full.py

--- clean_full.py
+++ clean_full.py
@@ -2,11 +2,11 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.7>
 html = requests.get('https://www.amd.com/en/support/kb/release-notes/rn-rad-win-'+sys.argv[1],headers=headers).content
 
 soup = BeautifulSoup(html, 'html.parser')
-a = soup.select("a[href*='/drivers/whql']")
-link = a.get('href')[0]
+a = soup.select('a[href*="/drivers/whql"]')
+link = a[0].get('href')
 
 print(link)
