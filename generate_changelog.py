b540430a82e5a9379d0a6518c4fac1bbbce86ce4
NunoDxxD <41538139+nunodxxd@users.noreply.github.com>
2023-04-21 13:07:56 +0100

    Update generate_changelog.py

--- generate_changelog.py
+++ generate_changelog.py
@@ -1,7 +1,9 @@
 import sys
+import os
 import requests
 from bs4 import BeautifulSoup
 import markdownify
+import hashlib
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
 html = requests.get(f'https://www.amd.com/en/support/kb/release-notes/rn-rad-win-{sys.argv[1]}',headers=headers).content
@@ -12,6 +14,23 @@ selected_html = ''
 for x in range(1,7):
         selected_html += str(soup.select(f'#block-amd-content > article > div > div.field.field--name-body.field--type-text-with-summary.field--label-hidden.field__item > *:nth-child({x})')[0])
 
+#hash generator
+exe_files = [f for f in os.listdir('./driver') if f.endswith('.exe')]
+
+for file in exe_files:
+    path_to_file = f'./driver/{file}'
+    hasher = hashlib.sha256()
+    with open(path_to_file, 'rb') as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hasher.update(chunk)
+    hash_value = hasher.hexdigest()
+    
+    # Imprime o hash SHA256 do arquivo
+    print(f'O hash SHA256 do arquivo {file} é: {hash_value}')
+
+    selected_html += f'<p>O hash SHA256 do arquivo {file} é: {hash_value}</p>'
+
+#export markdown
 markdown = markdownify.markdownify(selected_html, heading_style="ATX")
 
 print(markdown)
