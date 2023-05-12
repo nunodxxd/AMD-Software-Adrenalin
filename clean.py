3a16cf80004812347c348612bac73193a0d45375
NunoDxxD <41538139+nunodxxd@users.noreply.github.com>
2023-05-12 12:07:09 +0100

    Update clean.py

--- clean.py
+++ clean.py
@@ -4,9 +4,19 @@ from bs4 import BeautifulSoup
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
 html = requests.get('https://www.amd.com/en/support/graphics/amd-radeon-5700-series/amd-radeon-rx-5700-series/amd-radeon-rx-5700-xt',headers=headers).content
 
+# old method 
+# a = soup.select('#amd_support_driver_list > div > div > div > details > div > div > div > span > div > div > div > a')[2]
+# link = a.get('href')
+
 soup = BeautifulSoup(html, 'html.parser')
-#a = soup.select('#buttons > div > a')[0]
-a = soup.select('#amd_support_driver_list > div > div > div > details > div > div > div > span > div > div > div > a')[2]
-link = a.get('href')
+divs = soup.find_all('div', class_='field__item', text='Auto-Detect and Install')
+
+if divs:
+    link_div = divs[0].find_next('div', class_='field--type-uri')
+    link = link_div.find('a')['href']
+    print(link)
+else:
+    print("Div não encontrada")
+
 
 print(link)
