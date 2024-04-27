c87ef82110f56ddff3423931534cc7bb90d8b984
NunoDxxD <41538139+nunodxxd@users.noreply.github.com>
2024-04-27 02:53:03 -0400

    Update clean_full.py

--- clean_full.py
+++ clean_full.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
-html = requests.get(f'https://www.amd.com/en/support/kb/release-notes/rn-rad-win-{sys.argv[1]}',headers=headers).content
+html = requests.get(f'https://www.amd.com/en/resources/support-articles/release-notes/RN-RAD-WIN-{sys.argv[1]}.html',headers=headers).content
 
 soup = BeautifulSoup(html, 'html.parser')
 a = soup.select('a[href*="/drivers/"]')
