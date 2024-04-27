ebabda45bc34c5ad6d5511b86f725430b07f7f9c
NunoDxxD <41538139+nunodxxd@users.noreply.github.com>
2024-04-27 01:57:45 -0400

    Update generate_changelog.py

--- generate_changelog.py
+++ generate_changelog.py
@@ -10,7 +10,7 @@ html = requests.get(f'https://www.amd.com/en/support/kb/release-notes/rn-rad-win
 
 soup = BeautifulSoup(html, 'html.parser')
 
-selected_html = soup.select_one('div.center-container > div > div.cmp-container__content > div > div[data-cmp-data-layer]').decode_contents()
+selected_html = soup.select('div.center-container > div > div.cmp-container__content > div > div[data-cmp-data-layer]')[0].decode_contents()
 
 """
 for x in range(1,7):
