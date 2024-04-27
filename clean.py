eb6bde17f1ef3345f628cce29ce3cd0e9ea14d16
NunoDxxD <41538139+nunodxxd@users.noreply.github.com>
2024-04-27 00:35:20 -0400

    Update clean.py

--- clean.py
+++ clean.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
 html = requests.get('https://www.amd.com/en/support/graphics/amd-radeon-5700-series/amd-radeon-rx-5700-series/amd-radeon-rx-5700-xt',headers=headers).content
 
-# old method 
+# old method
+soup = BeautifulSoup(html, 'html.parser')
 a = soup.find_all('.container > div > div:nth-child(4) > div > a')[0]
 link = a.get('href')
 # print(link)
