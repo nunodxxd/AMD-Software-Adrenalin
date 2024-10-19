f09177e6a182d398cd0b5e8ca5f9a067261caff7
NunoDxxD <41538139+nunodxxd@users.noreply.github.com>
2024-10-19 11:02:00 +0100

    Update clean.py
    update gpu model

--- clean.py
+++ clean.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
-html = requests.get('https://www.amd.com/en/support/graphics/amd-radeon-5700-series/amd-radeon-rx-5700-series/amd-radeon-rx-5700-xt',headers=headers).content
+html = requests.get('https://www.amd.com/en/support/downloads/drivers.html/graphics/radeon-rx/radeon-rx-7000-series/amd-radeon-rx-7900-xt.html',headers=headers).content
 
 # old method
 soup = BeautifulSoup(html, 'html.parser')
